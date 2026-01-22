6a44046360720dcb0e8c345913ecb677daec0fc8
Nii <jnkpappoe@gmail.com>
2026-01-22 03:03:17 +0000

    adding life to artisan dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,47 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { AnimatePresence } from 'framer-motion'; // <--- FIX 1: Missing Import
 import 'react-toastify/dist/ReactToastify.css';
-import PaymentCallback from './pages/PaymentCallback'; // <--- Import
+
 // Import Pages
+import PaymentCallback from './pages/PaymentCallback';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import Navbar from './components/Navbar';
 import ProfileSetup from './pages/ProfileSetup';
 import ArtisanDashboard from './pages/ArtisanDashboard';
-import Home from './pages/Home'; // <--- IMPORT HOME
-import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
 import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+
+// <--- FIX 2: Location must be used INSIDE a sub-component or a wrapper
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        {/* Protected Routes */}
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/profile-setup" element={<ProtectedRoute role="artisan"><ProfileSetup /></ProtectedRoute>} />
+        <Route path="/artisan-dashboard" element={<ProtectedRoute role="artisan"><ArtisanDashboard /></ProtectedRoute>} />
+        <Route path="/admin-dashboard" element={<ProtectedRoute role="admin"><AdminDashboard /></ProtectedRoute>} />
+        
+        <Route path="/payment/callback" element={<PaymentCallback />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <ToastContainer position="top-right" autoClose={3000} />
-      <Routes>
-        {/* MAKE HOME THE DEFAULT ROUTE */}
-        <Route path="/" element={<Home />} /> 
-        
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile-setup" element={<ProfileSetup />} />
-        <Route path="/artisan-dashboard" element={<ArtisanDashboard />} />
-        <Route path="/payment/callback" element={<PaymentCallback />} />
-        <Route path="/admin-dashboard" element={<ProtectedRoute role="admin"> <AdminDashboard /> </ProtectedRoute>} />
-      </Routes>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
+      <AnimatedRoutes />
     </Router>
   );
 }
