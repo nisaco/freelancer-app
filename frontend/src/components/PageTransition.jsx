75a3ff334da805f69408e8d7ef5aa827bb34bc97
Nii <jnkpappoe@gmail.com>
2026-02-07 02:21:57 +0000

    fixing navbar stuff

--- frontend/src/components/PageTransition.jsx
+++ frontend/src/components/PageTransition.jsx
@@ -5,6 +5,7 @@ const PageTransition = ({ children }) => {
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
+      className="pt-24 md:pt-32"
       exit={{ opacity: 0, y: -20 }}
       transition={{ duration: 0.5, ease: "easeOut" }}
     >
